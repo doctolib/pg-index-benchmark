f9ab0b145e82043fd52d5be1348e9d723bbae5e5
Emmanuel Quincerot <emmanuel.quincerot@doctolib.com>
2023-03-28 15:46:38 +0200

    Improve tests (#9)
    Adding some extra tests and also extracting some methods.

--- test/pg_index_benchmark/cli/option_parser_test.rb
+++ test/pg_index_benchmark/cli/option_parser_test.rb
@@ -1,42 +1,47 @@
 # frozen_string_literal: true
 
-require_relative '../../test_helper.rb'
-class OptionParserTest < Minitest::Test
-  describe "benchmark mode" do
-    def test_default
-      assert_equal [{ mode: :benchmark }, []],
-                   ::PgIndexBenchmark::CLI::OptionParser.new.parse([])
+require_relative "../../test_helper.rb"
+module PgIndexBenchmark
+  class CLI::OptionParserTest < Minitest::Test
+    describe "benchmark mode" do
+      def test_default
+        assert_equal [{ mode: :benchmark }, []],
+                     ::PgIndexBenchmark::CLI::OptionParser.new.parse([])
+      end
+
+      def test_with_query
+        assert_equal [
+                       { mode: :benchmark, only_query_fingerprint: "123456" },
+                       []
+                     ],
+                     ::PgIndexBenchmark::CLI::OptionParser.new.parse(
+                       %w[-q 123456]
+                     )
+      end
+
+      def test_with_query_file
+        assert_equal [{ mode: :benchmark }, ["queries.sql"]],
+                     ::PgIndexBenchmark::CLI::OptionParser.new.parse(
+                       ["queries.sql"]
+                     )
+      end
+
+      def test_with_config_file
+        assert_equal [
+                       { mode: :benchmark, config_path: "foo_config.yml" },
+                       ["queries.sql"]
+                     ],
+                     ::PgIndexBenchmark::CLI::OptionParser.new.parse(
+                       %w[-c foo_config.yml queries.sql]
+                     )
+      end
     end
 
-    def test_with_query
-      assert_equal [{ mode: :benchmark, only_query_fingerprint: "123456" }, []],
-                   ::PgIndexBenchmark::CLI::OptionParser.new.parse(
-                     %w[-q 123456]
-                   )
-    end
-
-    def test_with_query_file
-      assert_equal [{ mode: :benchmark }, ["queries.sql"]],
-                   ::PgIndexBenchmark::CLI::OptionParser.new.parse(
-                     ["queries.sql"]
-                   )
-    end
-
-    def test_with_config_file
-      assert_equal [
-                     { mode: :benchmark, config_path: "foo_config.yml" },
-                     ["queries.sql"]
-                   ],
-                   ::PgIndexBenchmark::CLI::OptionParser.new.parse(
-                     %w[-c foo_config.yml queries.sql]
-                   )
-    end
-  end
-
-  describe "deduplicate mode" do
-    def test_runs_in_benchmark_mode
-      assert_equal [{ mode: :deduplicate, table_name: "foo" }, []],
-                   ::PgIndexBenchmark::CLI::OptionParser.new.parse(%w[-d foo])
+    describe "deduplicate mode" do
+      def test_runs_in_benchmark_mode
+        assert_equal [{ mode: :deduplicate, table_name: "foo" }, []],
+                     ::PgIndexBenchmark::CLI::OptionParser.new.parse(%w[-d foo])
+      end
     end
   end
 end
